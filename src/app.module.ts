e3441f950caec6d1e3ed913203308ffbb6a494f8
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2025-08-29 10:35:32 -0300

    Conectando ao Supabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsuariosModule } from './usuarios/usuarios.module';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from './ormconfig';
+import { Usuario } from './usuarios/entities/usuario.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DB_URL,
+      autoLoadEntities: true, // carrega todas as entidades automaticamente, usar apenas em dev
+      synchronize: true, // só use em dev, cria tabelas automaticamente
+      // entities: [Usuario] // usar em prod listando todas as entidades e remover autoLoadEntities
     }),
-    UsuariosModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -4,10 +4,5 @@ export const config: TypeOrmModuleOptions = {
   type: 'postgres',
   url: process.env.DB_URL,
   synchronize: true, // Não usar true em produção
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS, // A senha do Supabase
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
 };
